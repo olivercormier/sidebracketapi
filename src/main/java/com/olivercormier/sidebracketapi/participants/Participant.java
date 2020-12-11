ea700b21b7b35ee8002d64b7ff317e0e6106a7ce
Oliver Cormier <ocormier25@gmail.com>
2020-12-11 14:21:34 -0400

    ParticipantController Completed

--- src/main/java/com/olivercormier/sidebracketapi/participants/Participant.java
+++ src/main/java/com/olivercormier/sidebracketapi/participants/Participant.java
@@ -7,6 +7,9 @@ public class Participant {
     public int id;
     public int userId;
 
+    //Create and Update won't work without this (reason unknown)
+    public Participant() {}
+
     public Participant(int userId) {
         this.userId = userId;
     }
--- src/main/java/com/olivercormier/sidebracketapi/participants/ParticipantController.java
+++ src/main/java/com/olivercormier/sidebracketapi/participants/ParticipantController.java
@@ -1,9 +1,13 @@
 package com.olivercormier.sidebracketapi.participants;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.Collection;
+import java.net.URI;
+import java.util.Optional;
 
 @RestController
 public class ParticipantController {
@@ -14,8 +18,48 @@ public class ParticipantController {
         this.participants = new ParticipantDao();
     }
 
+    @GetMapping("/participants/{id}")
+    ResponseEntity<Object> read(@PathVariable("id") String id) {
+        Optional<Participant> result = participants.get(Integer.parseInt(id));
+        if (result.isPresent()) {
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Participant Record Not Found", HttpStatus.NOT_FOUND);
+        }
+    }
+
     @GetMapping("/participants")
-    Collection<Participant> all() {
-        return participants.getAll();
+    ResponseEntity<Object> all() {
+        return new ResponseEntity<>(participants.getAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/participants")
+    ResponseEntity<Object> create(@RequestBody Participant participant) {
+        HttpHeaders headers = new HttpHeaders();
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(participants.save(participant)).toUri();
+        headers.setLocation(location);
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/participants/{id}")
+    ResponseEntity<Object> update(@PathVariable("id") String id, @RequestBody Participant participant) {
+        participant.setId(Integer.parseInt(id));
+        participants.update(participant);
+        HttpHeaders headers = new HttpHeaders();
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(id).toUri();
+        headers.setLocation(location);
+        return new ResponseEntity<>(headers, HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/participants/{id}")
+    ResponseEntity<Object> delete(@PathVariable("id") String id) {
+        participants.delete(Integer.parseInt(id));
+        HttpHeaders headers = new HttpHeaders();
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(id).toUri();
+        headers.setLocation(location);
+        return new ResponseEntity<>(headers, HttpStatus.NO_CONTENT);
     }
 }
