f17b67bccc0411034b9101b0d4f9ad9c128a898f
Oliver Cormier <ocormier25@gmail.com>
2020-12-11 03:51:07 -0400

    ResultController Completed

--- src/main/java/com/olivercormier/sidebracketapi/results/ResultController.java
+++ src/main/java/com/olivercormier/sidebracketapi/results/ResultController.java
@@ -1,9 +1,13 @@
 package com.olivercormier.sidebracketapi.results;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.Collection;
+import java.net.URI;
+import java.util.Optional;
 
 @RestController
 public class ResultController {
@@ -14,8 +18,48 @@ public class ResultController {
         this.results = new ResultDao();
     }
 
+    @GetMapping("/results/{id}")
+    ResponseEntity<Object> read(@PathVariable("id") String id) {
+        Optional<Result> result = results.get(Integer.parseInt(id));
+        if (result.isPresent()) {
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Result Record Not Found", HttpStatus.NOT_FOUND);
+        }
+    }
+
     @GetMapping("/results")
-    Collection<Result> all() {
-        return results.getAll();
+    ResponseEntity<Object> all() {
+        return new ResponseEntity<>(results.getAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/results")
+    ResponseEntity<Object> create(@RequestBody Result result) {
+        HttpHeaders headers = new HttpHeaders();
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(results.save(result)).toUri();
+        headers.setLocation(location);
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/results/{id}")
+    ResponseEntity<Object> update(@PathVariable("id") String id, @RequestBody Result result) {
+        result.setId(Integer.parseInt(id));
+        results.update(result);
+        HttpHeaders headers = new HttpHeaders();
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(id).toUri();
+        headers.setLocation(location);
+        return new ResponseEntity<>(headers, HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/results/{id}")
+    ResponseEntity<Object> delete(@PathVariable("id") String id) {
+        results.delete(Integer.parseInt(id));
+        HttpHeaders headers = new HttpHeaders();
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(id).toUri();
+        headers.setLocation(location);
+        return new ResponseEntity<>(headers, HttpStatus.NO_CONTENT);
     }
 }
